c655b7406bd5d22e955713897a1ce8b21497375b
Edsel Caprice <caprice@mail.usf.edu>
2018-06-15 03:51:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,6 +25,95 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
+  def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board,index,current_player(board))
+    display_board(board)
+  else
+   turn(board)
+  end
+end
+
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+    WIN_COMBINATIONS.detect do |combination|
+  
+     if (board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X") ||
+         (board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O")
+        
+        return combination
+        
+        else
+          
+        false 
+    end
+  end
+end
+
+
+def full?(board)
+board.all? {|space| space == "X" || space == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+  end
+  
+def over?(board)
+  won?(board) || draw?(board)
+end
+  
+def winner(board)
+  if won?(board).class == Array
+    return board[won?(board)[0]]
+  else
+  return nil
+  end
+end
+
+
+  
+  def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
   
   
 end
